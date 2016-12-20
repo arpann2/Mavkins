062278a9cc3add4fd88c2763c617129d4f252023
Arpan <arpann2@gmail.com>
2016-12-20 00:46:29 +0000

    fourth commit.

--- src/test/java/FirstPage.java
+++ src/test/java/FirstPage.java
@@ -1,9 +1,7 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 /**
  * Created by arpan on 19/12/2016.
@@ -12,20 +10,28 @@ public class FirstPage {
 
 
     private  String firefox_driver_path = "E:\\JARs\\Selenium-3.0.1\\geckodriver-v0.11.1-win64\\geckodriver.exe";
+    private  String chrome_driver_path = "E:\\JARs\\Selenium-3.0.1\\chromedriver_win32\\chromedriver.exe";
     WebDriver driver;
 
-    @Test
+    @BeforeClass
     public void setUp () {
-        System.setProperty("webdriver.gecko.driver",firefox_driver_path);
-        driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver",chrome_driver_path);
+        driver = new ChromeDriver();
         driver.get("https://bbc.co.uk");
         driver.manage().window().maximize();
     }
 
     @Test
     public void verifyTitle() {
-        driver.getTitle();
+       String title = driver.getTitle();
+        System.out.println("title: " + title);
     }
+
+    @Test
+    public void verifyMethod() {
+        System.out.println("hello from github");
+    }
+
 
     @AfterClass
     public void tearDown() {
@@ -34,10 +40,7 @@ public class FirstPage {
 
 
 
-    @Test
-    public void verifyMethod() {
-        System.out.println("hello from github");
-    }
+
 
 
 
